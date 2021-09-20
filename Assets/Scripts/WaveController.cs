29d86bf21f079895f94d68a9cc14a4957f0d5eac
Runi <runi54rus@gmail.com>
2021-09-20 18:52:38 +0700

    spawner done

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class WaveController : MonoBehaviour
+{
+    public float ScoreMulti = 1;
+    public float HealthMulti = 1;
+    public float SpeedMulti = 1;
+    public List<WaveSettings> WaveSettings = new List<WaveSettings>();
+}
+
+[Serializable]
+public class WaveSettings
+{
+    public int Size;
+    public float Delay = 1f;
+    public List<EnemyGroup> EnemyGroups = new List<EnemyGroup>();
+}
